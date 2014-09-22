77259de30e8b5e52e36fe1eec06ad46d6a718d95
mbertelsen <mattias@spaceship.com>
2014-09-21 22:40:59 -0500

    stubbed out and mildly styled recipe list

--- src/main/webapp/js/app/recipes.js
+++ src/main/webapp/js/app/recipes.js
@@ -1,2 +1,12 @@
 
-angular.module('recipesApp', []);
+var app = angular.module('recipesApp', ['ui.bootstrap']);
+
+app.controller('RecipeListController', ['$scope', function($scope) {
+
+    $scope.recipes = [
+        { id: 1, name:'Foo', description:'Bar' },
+        { id: 2, name:'Garb', description:'Shnozzle' },
+        { id: 3, name:'Shit', description:'Bacon' }
+    ];
+
+}]);
